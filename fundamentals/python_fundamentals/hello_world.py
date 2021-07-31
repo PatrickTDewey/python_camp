d5e358095050c03ce8b12f3f8301736d071c1b8e
PatrickTDewey <ptddev@protonmail.com>
2021-07-31 16:36:40 -0700

    messed around with strings more

--- fundamentals/python_fundamentals/hello_world.py
+++ fundamentals/python_fundamentals/hello_world.py
@@ -1,16 +1,20 @@
 # 1. TASK: print "Hello World"
 print('Hello World')
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
+name = "Patrick"
 print('Hello', name)	# with a comma
 print('Hello ' + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
+favorite_num = 8
 print('Hello', name)	# with a comma
-# print('Hello'+name)	# with a +	-- this one should give us an error!
+print('Hello '+ str(favorite_num))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
-fave_food2 = "pizza"
+fave_food2 = "pizza chips"
 print('I love to eat {} and {}'.format(fave_food1, fave_food2)) # with .format()
 print(f'I love to eat {fave_food1} and {fave_food2}') # with an f string
-
+print(f'I love to eat {fave_food1.upper()} and {fave_food2.upper()}') # with an f string
+print(f'I love to eat {fave_food1.title()} and {fave_food2.title()}') # with an f string
+print(f'I love to eat {fave_food1.title()} and {fave_food2.join(fave_food1)}') # with an f string
+print(f'I love to eat {fave_food1.title()} and {fave_food2.split()}') # with an f string
