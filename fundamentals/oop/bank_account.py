710ef3106d30f0ea7caa79c27271ed44c329ef13
PatrickTDewey <ptddev@protonmail.com>
2021-08-01 19:03:32 -0700

    added bank_account multiple account functionality

--- fundamentals/oop/bank_account.py
+++ fundamentals/oop/bank_account.py
@@ -2,7 +2,8 @@ class BankAccount:
     # class attribute
     bank_name = 'First National Taco Bank'
     all_accounts = []
-    def __init__(self, int_rate, balance = 0):
+    def __init__(self, account_name, int_rate, balance = 0):
+        self.account_name = account_name
         self.int_rate = float(int_rate)
         self.balance = balance
         BankAccount.all_accounts.append(self)
@@ -13,7 +14,7 @@ class BankAccount:
         # we can use the static method here to evaluate 
         # if we can withdrawl the funds without going negative
         if BankAccount.can_withdraw(self.balance, amount):
-            self.balance -= amount
+                self.balance -= amount
         else:
             print('Insufficient funds: Charging a $5 fee')
             self.balance -= -5
@@ -48,7 +49,6 @@ class BankAccount:
             sum += account.balance
             
         return sum
-
     @classmethod
     def all_account_info(cls):
         # for i in range(len(cls.all_accounts)):
@@ -57,28 +57,57 @@ class BankAccount:
             print((i, v.display_account_info()))
        
 
-account_one = BankAccount(0.01, 100)
-account_two = BankAccount(0.03)
 
-account_two.deposit(100).deposit(100).deposit(100).withdraw(50).yield_interest()
-print(BankAccount.all_account_info())
+
 class User:
-    bank_name = 'Bank of Patricio'
     def __init__(self, name, email_address):
+        self.account_list = []
         self.name = name
         self.email_address = email_address
         # account balance is set to 0
-        self.account_balance = 0
-    def make_deposit(self, amount):
-        self.account_balance += amount
+    def create_account(self, name, int_rate, balance):
+        self.account = BankAccount(name, int_rate, balance)
+        self.account_info = {
+            'bank_name': self.account.bank_name,
+            'account_name': self.account.account_name,
+            'interest_rate': self.account.int_rate,
+            'balance': self.account.balance
+        }
+        self.account_list.append(self.account_info)
         return self
-    def make_withdrawl(self, amount):
-        self.account_balance -= amount
-        return self
+    def make_deposit(self, name, amount):
+        for i in range(len(self.account_list)):
+            if self.account_list[i]['account_name'] == name:
+                self.account.deposit(amount)
+                print(self.account.display_account_info())
+                self.account_list[i]['balance'] = self.account.balance
+                return self
+            else:
+                print('No account with that name found')
+                return self
+            
+    def make_withdrawl(self, name, amount):
+        for i in range(len(self.account_list)):
+            if self.account_list[i]['account_name'] == name:
+                self.account.withdraw(amount)
+                print(self.account.display_account_info())
+                self.account_list[i]['balance'] = self.account.balance
+                break
+            else:
+                print('No account with that name found')
+            return self
     def display_user_balance(self):
-        print(f'User: {self.name} \nBalance: ${self.account_balance}')
+        print(f'User: {self.name} \nBalance: ${self.account.balance}')
         return self
     def transfer_money(self, other_user, amount):
-        self.make_withdrawl(amount)
-        other_user.make_deposit(amount)
+        self.account.withdraw(amount)
+        other_user.account.deposit(amount)
         return self
+
+patrick = User('Patrick', 'ptddev@protonmail.com')
+patrick.create_account('Checking', 0.01, 100)
+patrick.create_account('Savings', 0.05, 300)
+patrick.make_deposit('Checking', 100).make_withdrawl('Checking', 100)
+print(patrick.account_list)
+# print(patrick.display_user_balance())
+# print(patrick.account_list)
