0c445c5fb1b958a2e345f41fb7de773bbb768814
PatrickTDewey <ptddev@protonmail.com>
2021-08-03 15:51:13 -0700

    added table assignment and used bootstrap

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -9,7 +9,15 @@ def hello_world():
     # return 'Hello World'  # Return "Hello World as a response"
     return render_template("index.html", phrase = "hello", steps = 5)
 
-
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("list.html", random_numbers = [3,1,5], students = student_info)
 @app.route('/success')
 def success_route():
     return 'Success Route'
@@ -26,6 +34,7 @@ def show_user_profile(username, id):
     print(username)
     print(id)
     return "username:" + username + ", id: " + id
+
 
 if __name__ == "__main__":  # Ensure this file is being run directly and not from a different module
     app.run(debug=True)  # Run the app in debug mode
