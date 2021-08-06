5c4d1ae3de7ec2013b2a439667e86253565145b4
PatrickTDewey <ptddev@protonmail.com>
2021-08-05 21:03:40 -0700

    finished dojo_survey assignment

---
+++ flask/fundamentals/dojo_survey/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "This is a freaking secret key, ok?"
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    session['form'] = request.form
+    print(session['form'])
+
+    return redirect('/info')
+
+@app.route('/info')
+def info():
+    
+    return render_template("info.html")
+if __name__ == "__main__":
+    app.run(debug=True)
