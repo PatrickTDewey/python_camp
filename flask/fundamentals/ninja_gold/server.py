cf2eb9c310ae636d2c2e33b8e313c80f7d1749d9
PatrickTDewey <ptddev@protonmail.com>
2021-08-06 11:23:27 -0700

    added ninja gold attempts and reset button

--- flask/fundamentals/ninja_gold/server.py
+++ flask/fundamentals/ninja_gold/server.py
@@ -5,13 +5,21 @@ import random
 app = Flask(__name__)
 app.secret_key = "The secret is awesome, you should know"
 
+my_dict = {}
 
 @app.route('/')
 def index():
-
+    if 'tries' not in session:
+        session['tries'] = int(0)
+    elif int(session['tries']) == 15 or int(session['gold'] >= 300):
+        if int(session['gold']) >= 300 and int(session['tries']) <= 15:
+            session['win/lose'] = 'won'
+        else:
+            session['win/lose'] = 'lost'
+        session['switch'] = True
     if 'gold' not in session:
         session['gold'] = 0
-
+    
     return render_template("index.html")
 
 
@@ -28,27 +36,39 @@ def process():
         session["message"].append(
             f"<ul><li class='green'>Earned {gold_return} golds from the cave on {dt_string}</li></ul>")
         session['gold'] += gold_return
+        session['tries'] += 1
     elif session['value'] == 'farm':
         gold_return = random.randint(10, 20)
         session["message"].append(
             f"<ul><li class='green'>Earned {gold_return} golds from the house on {dt_string}</li></ul>")
         session['gold'] += gold_return
+        session['tries'] += 1
     elif session['value'] == 'house':
         gold_return = random.randint(2, 5)
         session["message"].append(
             f"<ul><li class='green'>Earned {gold_return} golds from the house on {dt_string}</li></ul>")
         session['gold'] += gold_return
+        session['tries'] += 1
     elif session['value'] == 'casino':
         gold_return = random.randint(-50, 50)
         if gold_return < 0:
             session["message"].append(
                 f"<ul><li class='red'>Lost {gold_return * -1} golds from the casino on {dt_string}</li></ul>")
             session['gold'] += gold_return
+            session['tries'] += 1
         else: 
             session["message"].append(
                 f"<ul><li class='green'>Gained {gold_return} golds from the casino on {dt_string}</li></ul>")
             session['gold'] += gold_return
+            session['tries'] += 1
     print(session['message'])
+    
+    return redirect('/')
+
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session.clear();
     return redirect('/')
 
 
